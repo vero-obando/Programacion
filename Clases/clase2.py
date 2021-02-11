f80fa9075b10714f752012fd520523af304e19c6
vero-obando <obando.veronica@uces.edu.co>
2021-02-11 07:43:03 -0500

    preguntas verdadero y falso

---
+++ Clases/clase2.py
@@ -0,0 +1,27 @@
+pruebaV = True
+pruebaF = False
+print (pruebaF)
+print (pruebaV)
+pruebaV = pruebaF
+print (pruebaV)
+edad = 18
+estatura = 1.55 
+peso = 55 
+NOMBRE = "Veronica Obando Builes"
+print ("#"*15, "Mayor Edad", "#"*15)
+isMayorEdad = edad >= 18
+print (isMayorEdad)
+print ("#"*15, "Bajo La Estatura Promedio", "#"*15)
+iaMayorEstatura = estatura < 1.70
+# Calculando si el peso es diferente de 55
+print (iaMayorEstatura)
+print ("#"*15, "Peso diferente 55", "#"*15)
+isPesoDiferente = peso != 55
+print (isPesoDiferente)
+# Vamos a ver si el apellido esta en el nombre completo 
+apellido = "Obando"
+isApellido = apellido in NOMBRE 
+print ("#"*15, "Esta El Apellido En El Nombre", "#"*15)
+print (isApellido)
+
+
---
+++ Clases/operaciones.py
@@ -0,0 +1,13 @@
+numeroA = 87
+numeroB = 83
+sumar = numeroA + numeroB
+print ("el resultado de la suma es", sumar)
+print (f"la suma dio {sumar} exitosamente ")
+resta = numeroA - numeroB
+print ("el resultado de la resta es", resta)
+multiplicar = numeroA * numeroB
+print ("el resultado de la multiplicacion es", multiplicar )
+dividir = numeroA / numeroB
+print (f"la division dio {dividir} exitosamente ")
+exponente = numeroA ** numeroB
+print ("el resultado es", exponente)
